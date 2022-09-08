43ec27e199bbd561cfa3f2cff5473518d24f027c
Rick Borgen <Richard.L.Borgen@jpl.nasa.gov>
2022-09-07 17:17:56 -0700

    add automated fix to App.jsx for latest version num

--- editor/make.sh
+++ editor/make.sh
@@ -17,19 +17,25 @@
 echo 'Staging current json config files.'
 cp -p ../json/*json src
 
+# get the current release version
+toolver=$(./toolver.py)
+
+echo 'Fixing the version in App.jsx'
+echo 'WAS:'
+grep "Configuration Editor" src/App.jsx
+sed -i ""  "s/Editor.*<\/h3>/Editor  $toolver<\/h3>/" src/App.jsx
+echo 'IS:'
+grep "Configuration Editor" src/App.jsx
+
 # use node package builder (npm) to build editor
 echo 'Running npm build.'
 npm run build
 
-#  changed to a local reference within the build directory
 echo 'Editing the index.html'
-cat build/index.html | sed -e 's/\/static/.\/static/g' > yyy
+#  Fix the subdirectory references to be relative
+sed -i "" 's/\/static/.\/static/g' build/index.html
 #
 #  Change the browser tab title to the tool name & version
-toolver=$(./toolver.py)
-cat yyy | sed -e "s/React App/IonConfig $toolver/" > zzz
-
-mv zzz build/index.html
-/bin/rm yyy
+sed -i ""  "s/React App/IonConfig $toolver/" build/index.html
 
 echo 'Done.'
