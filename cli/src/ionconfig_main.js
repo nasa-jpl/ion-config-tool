da37affb9f58a1199d2c35461b35cf3e52901c76
dhanks <David.R.Hanks@jpl.nasa.gov>
2023-11-16 08:43:29 -0800

    Correct fix to issue #6

--- cli/src/ionconfig_main.js
+++ cli/src/ionconfig_main.js
@@ -4,9 +4,21 @@
 //
 //  generate full set of ion config files based on ion model
 //
-//  usage:  ionconfig.js -m [ionmodelfile]
+//  usage:  ionconfig.js -m [ionmodelfile] [-w configfiles]
 //
 //  inputs: ion_model.json  (or alternative name)
+//
+//  options:
+//           -w configfiles  configs files is a list of the config file types
+//                           for which the watch flag should be set to enable
+//                           watch characters in ION. Valid values are:
+//                           all - set the watch in all config files that 
+//                                 support it.
+//                           bp - set the watch in all versions of bprc
+//                           bssp - set the watch in bssprc
+//                           cfdp - set the watch in cfdprc
+//                           dtpc - set the watch in dtpcrc
+//                           ltp - set the watch in ltprc
 //
 //  outputs: ion_model directory,    (using model name)
 //           ion_model/node1 subdir, (per node using node names from model)
@@ -49,6 +61,9 @@ var cloneValues = {}
 
 var DEBUG_MODE = false;  // No debug messages to the console is default.
 
+var watchables = ["all", "cfdp", "ltp", "bp", "bssp", "dtpc"];
+// watch flag initialization, passed in on command line
+
 console.log("Build ION Configurations from an ION Model");
 
 var argv = mm(process.argv.slice(2));
@@ -65,6 +80,52 @@ try {
   console.log("" + err);
   process.exit();
 }
+
+
+// process any requests to set watch flags
+var wflags = {};
+var w = argv.w 
+
+if (w) {
+  var wflags = w.split(" ");
+
+  // exit on unrecognized watch flag, no guessing what user wants
+  for (i = 0; i < wflags.length; i++) {
+    if (!watchables.includes(wflags[i])) {
+      console.log("Unrecognized watch flag request: " + wflags[i]);
+      process.exit();
+    }
+  }
+
+  // Set up the watch commands object for easy access later
+  if (wflags.includes("all")) {
+    wflags.cfdprc={"type":"cfdprc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    wflags.ltprc={"type":"ltprc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    wflags.bpv7rc={"type":"bpv7rc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    wflags.bpv6rc={"type":"bpv6rc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    wflags.bssprc={"type":"bssprc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    wflags.dtpcrc={"type":"dtpcrc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    wcfgs = ["cfdprc","ltprc","bpv6rc","bpv7rc","bssprc","dtprc"];
+  } else {
+    if (wflags.includes("cfdp")) {
+      wflags.cfdprc={"type":"cfdprc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    }
+    if (wflags.includes("ltp")) {
+      wflags.ltprc={"type":"ltprc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    }
+    if (wflags.includes("bp")) {
+      wflags.bpv7rc={"type":"bpv7rc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+      wflags.bpv6rc={"type":"bpv6rc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    }
+    if (wflags.includes("bssp")) {
+      wflags.bssprc={"type":"bssprc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    }
+    if (wflags.includes("dtpc")) {
+      wflags.dtpcrc={"type":"dtpcrc_watch_all","lastUpdate":"2000-01-01T00:00","parameters":[]};
+    }
+  }
+}
+
 console.log("JSON parsing successful.");
 console.log("---");
 
