fa632ad17799f79de43c9ad7f1944468f059072c
dhanks <David.R.Hanks@jpl.nasa.gov>
2025-05-07 06:17:03 -0700

    Initial skeleton for automated test suite

---
+++ editor/test/NetAddr.test.js
@@ -0,0 +1,11 @@
+//        NetAddr.test.js     automated tests for NetAddr.jsx
+//
+//        Copyright (c) 2023, California Institute of Technology.
+//        ALL RIGHTS RESERVED.  U.S. Government Sponsorship
+//        acknowledged.
+//                                                                   
+//      Author: Dave Hanks, Jet Propulsion Laboratory         
+//                                                          
+
+import NetAddr from '../src/NetAddr';
+
