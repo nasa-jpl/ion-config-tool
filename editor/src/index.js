deb37a31e8a9a393c2f48236dcdfbb746e327d28
dhanks <David.R.Hanks@jpl.nasa.gov>
2025-06-05 12:01:43 -0700

    First basic changes for upgrade to React 19

--- editor/src/index.js
+++ editor/src/index.js
@@ -1,8 +1,10 @@
+import { createRoot } from "react-dom/client";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
 registerServiceWorker();
