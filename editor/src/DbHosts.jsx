a4e592e96487bbeca02425d004bc17f424a5c6c4
dhanks <David.R.Hanks@jpl.nasa.gov>
2025-08-15 12:43:57 -0700

    The host IPs are now being picked up from the db and associated with each host. Still have to render it in HTML.

--- editor/src/DbHosts.jsx
+++ editor/src/DbHosts.jsx
@@ -9,14 +9,13 @@
 
 import { useEffect, useState } from "react";
 import {Form,Container} from 'react-bootstrap';
-import {Row, Col} from 'react-bootstrap';
 import {Button,ButtonGroup} from 'react-bootstrap';
 import {Table} from 'react-bootstrap';
 import {Alert} from 'react-bootstrap';
 
 import nodeDB  from './json/nodeDB.json';
 
-// NodeDb *must* be a functional component as opposed to a
+// DbHosts *must* be a functional component as opposed to a
 // React component (ie. inherits from React.Component).
 // This is because NodeDb uses hooks.
 // 
@@ -28,29 +27,28 @@ import nodeDB  from './json/nodeDB.json';
 
 
 const DbHosts = () => {
-    const [theUrl, setUrl] = useState(null);
-    const [theContent, setContent] = useState("None");
-    const [theHostList, setHostList] = useState(null);
+    const [theHostTable, setHostTable] = useState(null);
+    const [hostList, setHostList] = useState([]);
+    const [hostsLoaded, setHostsLoaded] = useState(false);
 
+    // Extract the parent URLs from the JSON configs
     var dbHost = nodeDB["nodeDbUrls"].dbHost;
     var hostUrl = dbHost+nodeDB["nodeDbUrls"].hostUrl;
-    console.log(hostUrl);
 
-    //const {error, data: content} = DbContent("localhost:5000");
-
-    const fetchData = (theUrl) => {
-      if (!theUrl) return;
-      fetch(theUrl)
+    // Fetch hosts from the DB
+    const fetchHosts = (hostUrl) => {
+      fetch(hostUrl)
       .then(res => {
-        if (!res.ok) { // error coming back from server
-          throw Error('could not fetch the data for that resource');
+        if (!res.ok) { 
+          throw Error('could not fetch the data for hosts');
         }
-        console.log(res);
         return res.json();
       })
       .then(data => {
-        formatHostTable(data);
-        setContent(JSON.stringify(data));
+        // Successful fetch, set the toggle and pre process the data
+        setHostsLoaded(true);
+        formatHostTable(data); // Move this later
+        setHostList(preProcessHosts(data));
       })
       .catch(err => {
         if (err.name === 'AbortError') {
@@ -64,11 +62,87 @@ const DbHosts = () => {
       });
     };
 
-    // Make sure to get data on the first render
+    // Fetch host destinations
+    const fetchDestsWithIds = (destUrls) => { 
+      const destWithIds = destUrls.map(dest => (
+        {
+          dest: dest.host_id,
+          promise: fetch(dest.destUrl).then(res => res.json())
+        }
+      ));
+
+      Promise.all(destWithIds.map(item => item.promise))
+      .then(results => {
+        const dataWithIds = results.map((data, index) => ({
+          host_id: destWithIds[index].dest,
+          data: data
+        }));
+        console.log(dataWithIds);
+        setDestinationsInHosts(dataWithIds);
+      })
+      .catch(err => {
+        console.error(err);
+      });
+    }
+
+    // useEffect gets called after each time the object is rendered
+    // and if a dependency changes state.
+    //
+    // dependencies:
+    //    hostsLoaded -- toggle indicating whether hosts have been loaded from the db 
     useEffect(() => {
-      fetchData(hostUrl);
-    }, [])
-    
+      // Load the hosts from the db first
+      if (!hostsLoaded) {
+        console.log("going to load the hosts");
+        fetchHosts(hostUrl);
+      }
+      
+      // If the hosts are loaded, get the destinations (IP addreses)
+      if (hostsLoaded) {
+        var destUrls = [];
+        // Build up a set of URLs to do a batch fetch of all the IP
+        // addresses at once
+        for (let i=0 ; i<hostList.items.length; i++) {
+          let host_id = hostList.items[i].host_id;
+          let destUrl = hostUrl+"/"+host_id+"/destinations";
+          let urlWithHostId = {"host_id" : host_id, "destUrl" : destUrl };
+          destUrls.push(urlWithHostId);
+        }
+
+        // Do the fetch
+        fetchDestsWithIds(destUrls);
+      }
+    }, [hostsLoaded])
+
+    function setDestinationsInHosts(destData) {
+      destData.forEach(dataobj => {
+        let host_id = dataobj.host_id;
+        let dests = dataobj.data.items;
+        dests.forEach(element => {
+          const index = hostList.items.findIndex(item => item.host_id === host_id);
+          if (index !== -1) {
+            hostList.items[index].dests.push(element.destination_value);
+          }
+        })
+      })
+    };
+
+    // preProcessHosts
+    //
+    // Set up the host list to contain some data specific to the client
+
+    function preProcessHosts(data) {
+        var hosts = data;
+        // selected -- true = import host; false = do not import host
+        // dests -- array of destinations that is likely IPs associated with host
+        for (let i=0 ; i<hosts.items.length; i++) {
+            hosts.items[i].selected = false;
+            hosts.items[i].dests = [];
+        }
+        
+        return hosts;
+    }
+
     function formatHostTable(hostList) {
       var hostTable = "";
 
@@ -79,6 +153,7 @@ const DbHosts = () => {
               <th className="d-flex justify-content-center">Select</th>
               <th>Host ID</th>
               <th>Host Name</th>
+              <th>IP Addresses</th>
             </tr>
           </thead>
           <tbody>
@@ -94,12 +169,13 @@ const DbHosts = () => {
                 </td>
                 <td>{item.host_id}</td>
                 <td>{item.hostname}</td>
+                <td>100.4.5.1, 100.5.4.2</td>
               </tr>
             ))}
           </tbody>
         </Table>
 
-      setHostList(hostTable);
+      setHostTable(hostTable);
 
       //hostList.items.forEach(item => {
       //  console.log('host_id: '+item.host_id);
@@ -116,13 +192,14 @@ const DbHosts = () => {
 
     return(
       <>
+        {theHostTable && 
         <Container fluid>
-           {theHostList}
+           {theHostTable}
             <ButtonGroup>
               <Button variant="primary" onClick={importHosts}>Import</Button>
             </ButtonGroup>
 
-        </Container>
+        </Container>}
       </>
    );
 
