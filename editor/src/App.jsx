003907b61b2165862f61237ce6ac44958eee6119
dhanks <David.R.Hanks@jpl.nasa.gov>
2026-04-09 08:46:41 -0700

    Updated for latest build

--- editor/src/App.jsx
+++ editor/src/App.jsx
@@ -1863,7 +1863,7 @@ export default class App extends React.Component {
     <div>
       <Container fluid>
         <Navbar bg="light" variant="light">
-          <h3>ION Configuration Editor  4.9.0b4</h3>
+          <h3>ION Configuration Editor  4.9.0b5</h3>
         </Navbar>
         <Tabs
           className="mb=3"
